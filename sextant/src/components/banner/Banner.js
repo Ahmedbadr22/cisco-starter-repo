5f663e062ea7e6b8552037c63eca07a0150f8e0c
Ahmedbadr22 <220190144@fa-hists.edu.eg>
2022-07-11 22:07:52 +0200

    design frontend

---
+++ sextant/src/components/banner/Banner.js
@@ -0,0 +1,11 @@
+import "./banner-style.css";
+
+const Banner = () => {
+    return (
+        <div className="main-banner">
+            <h1>Sextent</h1>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ sextant/src/components/data-list/DataList.js
@@ -0,0 +1,14 @@
+import "./data-list-style.css";
+
+const DataList = () => {
+    return (
+        <div className="data-list">
+            <h3>Data List</h3>
+            <div className="list">
+                
+            </div>
+        </div>
+    );
+};
+
+export default DataList;
